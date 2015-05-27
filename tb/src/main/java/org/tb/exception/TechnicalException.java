7fb6f45c45abc9c9d8fd4b82b37b33db95fb2f82
Kai Dittmer <kd@hbt.de>
2015-05-27 12:22:01 +0000

    removed causes of eclipse warnings

--- tb/src/main/java/org/tb/exception/TechnicalException.java
+++ tb/src/main/java/org/tb/exception/TechnicalException.java
@@ -1,7 +1,8 @@
 package org.tb.exception;
 
 public abstract class TechnicalException extends RuntimeException {
-	
+	private static final long serialVersionUID = -4369282670953242771L;
+
 	public TechnicalException(String message) {
 		super(message);
 	}
