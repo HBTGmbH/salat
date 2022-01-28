d7546c25074f742c28ab68e6ca72f83512d25910
Josias Polchau <jp@hbt.de>
2022-01-28 21:07:23 +0100

    repair AuthenticationService and fill AuthenticationInterceptor usefully

--- src/main/java/org/tb/restful/auth/AuthenticationInterceptor.java
+++ src/main/java/org/tb/restful/auth/AuthenticationInterceptor.java
@@ -1,41 +1,30 @@
 package org.tb.restful.auth;
 
-import org.jboss.resteasy.core.Headers;
-import org.jboss.resteasy.core.ServerResponse;
-import org.tb.util.SecureHashUtils;
-
+import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
-import java.io.IOException;
-import java.util.List;
+import org.jboss.resteasy.core.Headers;
+import org.jboss.resteasy.core.ServerResponse;
+import org.tb.bdom.Employee;
 
 @Provider
 public class AuthenticationInterceptor implements ContainerRequestFilter {
 
-    private static final ServerResponse ACCESS_DENIED = new ServerResponse("Access denied for this resource", 401, new Headers<Object>());
-    @Context
-    private HttpServletRequest servletRequest;
+  private static final ServerResponse ACCESS_DENIED = new ServerResponse("\"Access denied for this resource\"", 401,
+      new Headers<Object>());
+  @Context
+  private HttpServletRequest servletRequest;
 
-    @Override
-    public void filter(final ContainerRequestContext containerRequestContext) throws IOException {
-        /* WTF?
-        final MultivaluedMap<String, String> headers = containerRequestContext.getHeaders();
-        List<String> xsrfHeader = headers.get("X-XSRF-TOKEN");
-        if (xsrfHeader != null && xsrfHeader.size() == 1) {
-            String xsrfToken = xsrfHeader.get(0);
-            Long employeeId = (Long) servletRequest.getSession().getAttribute("employeeId");
-            String salt = (String) servletRequest.getSession().getAttribute("jaxrs.salt");
-            String compareToken = SecureHashUtils.makeMD5(employeeId + "." + salt);
-            if (xsrfToken.equals(compareToken)) {
-                return;
-            }
-        }
-        containerRequestContext.abortWith(ACCESS_DENIED);
-         */
-    }
+  @Override
+  public void filter(final ContainerRequestContext containerRequestContext) throws IOException {
+    if(containerRequestContext.getUriInfo().getRequestUri().getPath().startsWith("/tb/rest/AuthenticationService/authenticate"))
+      return;
+    if (servletRequest.getSession().getAttribute("employeeId") == null) {
+          containerRequestContext.abortWith(ACCESS_DENIED);
+      }
+  }
 
 }
--- src/main/java/org/tb/restful/auth/AuthenticationService.java
+++ src/main/java/org/tb/restful/auth/AuthenticationService.java
@@ -1,8 +1,11 @@
 package org.tb.restful.auth;
 
+import lombok.Setter;
+import org.tb.GlobalConstants;
 import org.tb.bdom.Employee;
-import org.tb.persistence.EmployeeDAO;
-import org.tb.persistence.EmployeecontractDAO;
+import org.tb.bdom.Employeecontract;
+import org.tb.helper.AfterLogin;
+import org.tb.persistence.*;
 import org.tb.util.SecureHashUtils;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,30 +18,55 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
 import java.util.Date;
+import java.util.Map;
 import java.util.UUID;
 
-@Path("/rest/AuthenticationService")
+@Path("/rest/AuthenticationService")@Setter
 public class AuthenticationService {
 
     private EmployeecontractDAO employeecontractDAO;
     private EmployeeDAO employeeDAO;
+    private EmployeeorderDAO employeeorderDAO;
+    private PublicholidayDAO publicholidayDAO;
+    private TimereportDAO timereportDAO;
+    private OvertimeDAO overtimeDAO;
+
 
     @GET
     @Path("authenticate")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response authenticate(@Context HttpServletRequest request, @QueryParam("username") String username, @QueryParam("password") String password) {
+    public Response authenticate(@Context HttpServletRequest request, @QueryParam("username") String username,
+                                 @QueryParam("password") String password) {
 
         Employee employee = employeeDAO.getLoginEmployee(username, SecureHashUtils.makeMD5(password));
 
         if (employee != null) {
             Long employeeId = employee.getId();
             Date date = new Date();
-            Long employeecontractId = employeecontractDAO.getEmployeeContractByEmployeeIdAndDate(employeeId, date).getId();
-            request.getSession().setAttribute("employeeId", employeeId);
-            request.getSession().setAttribute("employeecontractId", employeecontractId);
 
+            Employeecontract employeecontract = employeecontractDAO.getEmployeeContractByEmployeeIdAndDate(
+                    employee.getId(), date);
+
+            if (employeecontract == null && !employee.getStatus()
+                    .equalsIgnoreCase(GlobalConstants.EMPLOYEE_STATUS_ADM)) {
+                return Response.noContent().status(401).build();
+            }
+            if (employeecontract != null) {
+                Long employeecontractId = employeecontract.getId();
+                request.getSession().setAttribute("employeeId", employeeId);
+                request.getSession().setAttribute("employeecontractId", employeecontractId);
+            }
             String salt = UUID.randomUUID().toString();
             request.getSession().setAttribute("jaxrs.salt", salt);
+
+            Map<String, Object> attributes = employeeDAO.getAttributes(request, employee);
+            attributes.forEach((key, value) -> request.getSession().setAttribute(key, value));
+
+            AfterLogin.handleOvertime(employeecontract, employeeorderDAO, publicholidayDAO, timereportDAO, overtimeDAO,
+                    request.getSession());
+
+
+            //  TODO Here are severel parts missing from LoginEmployeeAction
 
             // XSRF-TOKEN must be read from Client and be put into a HTTP-header
             // X-XSRF-TOKEN or as a query param named XSRF_TOKEN
