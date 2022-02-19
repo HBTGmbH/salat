0edf3336cbbc224196de24cd6291507ac1c6fad1
Klaus Richarz <kr@hbt.de>
2022-02-19 15:17:47 +0100

    [#44] Move to spring boot - remove spring-struts integration and introduce own one

---
+++ src/main/java/org/tb/SalatApplication.java
@@ -0,0 +1,21 @@
+package org.tb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableJpaRepositories
+@EnableJpaAuditing(auditorAwareRef = "loginEmployeeAuditorAware")
+@EnableTransactionManagement
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
+public class SalatApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SalatApplication.class, args);
+  }
+
+}
---
+++ src/main/java/org/tb/struts/DelegatingRequestProcessor.java
@@ -0,0 +1,62 @@
+package org.tb.struts;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionServlet;
+import org.apache.struts.action.RequestProcessor;
+import org.apache.struts.config.ModuleConfig;
+import org.springframework.beans.BeansException;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+@Slf4j
+public class DelegatingRequestProcessor extends RequestProcessor {
+
+  private WebApplicationContext webApplicationContext;
+
+  public void init(ActionServlet actionServlet, ModuleConfig moduleConfig) throws ServletException {
+    super.init(actionServlet, moduleConfig);
+    if (actionServlet != null) {
+      this.webApplicationContext = this.initWebApplicationContext(actionServlet);
+    }
+  }
+
+  protected WebApplicationContext initWebApplicationContext(ActionServlet actionServlet) throws IllegalStateException {
+    return WebApplicationContextUtils.getRequiredWebApplicationContext(actionServlet.getServletContext());
+  }
+
+  protected final WebApplicationContext getWebApplicationContext() {
+    return this.webApplicationContext;
+  }
+
+  protected Action processActionCreate(HttpServletRequest request, HttpServletResponse response, ActionMapping mapping) throws IOException {
+    Action action = this.getDelegateAction(mapping);
+    if(action == null) {
+      throw new RuntimeException("ouch! no bean found for action with id " + mapping.getActionId() + "(Path=" + mapping.getPath() + ")");
+    }
+    return action;
+  }
+
+  protected Action getDelegateAction(ActionMapping mapping) throws BeansException {
+    Class<?> actionClassType = this.determineActionClass(mapping);
+    return (Action) this.getWebApplicationContext().getBean(actionClassType);
+  }
+
+  @SneakyThrows
+  public static Class<?> determineActionClass(ActionMapping mapping) {
+    if(mapping.getType() == null) {
+      throw new RuntimeException("Missing type attribute in struts-config.xml action declaration for path " + mapping.getPath());
+    }
+    String actionClassName = mapping.getType();
+    return ClassUtils.forName(actionClassName, Thread.currentThread().getContextClassLoader());
+  }
+
+}
