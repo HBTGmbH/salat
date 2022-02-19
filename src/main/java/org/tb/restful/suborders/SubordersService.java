f09019f4542eb61ddc49b8f51c8fca7330e17e10
Klaus Richarz <kr@hbt.de>
2022-02-19 16:29:29 +0100

    [#44] Use AuthorizedUser in rest services to ensure authorization of requesting user

--- src/main/java/org/tb/restful/suborders/SubordersService.java
+++ src/main/java/org/tb/restful/suborders/SubordersService.java
@@ -1,6 +1,8 @@
 package org.tb.restful.suborders;
 
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 import java.util.ArrayList;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.tb.bdom.AuthorizedUser;
 import org.tb.bdom.Employeecontract;
 import org.tb.bdom.Suborder;
 import org.tb.persistence.EmployeecontractDAO;
@@ -25,26 +29,30 @@ public class SubordersService {
 
     private final EmployeecontractDAO employeecontractDAO;
     private final SuborderDAO suborderDAO;
+    private final AuthorizedUser authorizedUser;
 
     @GetMapping(path = "/availableSuborders", produces = APPLICATION_JSON_VALUE)
     @ResponseStatus(OK)
     public List<SuborderData> getAvailableSuborders(
-        @RequestParam("refDate") Date refDate,
-        @RequestParam("employeeId") long employeeId
+        @RequestParam("refDate") Date refDate
     ) {
+        if(!authorizedUser.isAuthenticated()) {
+            throw new ResponseStatusException(UNAUTHORIZED);
+        }
+
         if (refDate == null) refDate = DateUtils.today();
-
-        // FIXME check that provided employeeId matches the authenticated user
-
-        Employeecontract ec = employeecontractDAO.getEmployeeContractByEmployeeIdAndDate(
-            employeeId,
+        Employeecontract employeecontract = employeecontractDAO.getEmployeeContractByEmployeeIdAndDate(
+            authorizedUser.getEmployeeId(),
             refDate
         );
+        if(employeecontract == null) {
+            throw new ResponseStatusException(NOT_FOUND);
+        }
 
         // The method getSubordersByEmployeeContractIdWithValidEmployeeOrders
         // was added to the SuborderDao class!!!
         List<Suborder> suborders = suborderDAO.getSubordersByEmployeeContractIdWithValidEmployeeOrders(
-            ec.getId(),
+            employeecontract.getId(),
             refDate
         );
 
