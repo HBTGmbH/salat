563aa006deb95ffb448dc440b05dfc606ca94ee2
Klaus Richarz <kr@hbt.de>
2022-02-20 21:06:11 +0100

    [#44] Fix StatusReportDAO functionality

--- src/main/java/org/tb/persistence/StatusReportDAO.java
+++ src/main/java/org/tb/persistence/StatusReportDAO.java
@@ -1,67 +1,72 @@
 package org.tb.persistence;
 
-import java.util.Collections;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import static java.lang.Boolean.TRUE;
+import static java.util.Comparator.comparing;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.tb.bdom.Customerorder;
 import org.tb.bdom.Employee;
 import org.tb.bdom.Statusreport;
-
-import java.util.Date;
-import java.util.List;
+import org.tb.util.DateUtils;
 
 @Component
-public class StatusReportDAO extends AbstractDAO {
+@RequiredArgsConstructor
+public class StatusReportDAO {
 
     private final StatusreportRepository statusreportRepository;
-
-    @Autowired
-    public StatusReportDAO(SessionFactory sessionFactory,
-        StatusreportRepository statusreportRepository) {
-        super(sessionFactory);
-        this.statusreportRepository = statusreportRepository;
-    }
 
     /**
      * Gets the {@link Statusreport} for the given id.
      */
     public Statusreport getStatusReportById(long id) {
-        return (Statusreport) getSession().createQuery("from Statusreport sr where sr.id = ?").setLong(0, id).uniqueResult();
+        return statusreportRepository.findById(id).orElse(null);
     }
 
     /**
      * Get a list of all {@link Statusreport}s.
      */
-    @SuppressWarnings("unchecked")
     public List<Statusreport> getStatusReports() {
-        return getSession().createQuery("from Statusreport sr order by sr.customerorder.sign asc, sr.sort asc, sr.fromdate asc, sr.untildate asc, sr.sender.sign asc").list();
+        return StreamSupport.stream(statusreportRepository.findAll().spliterator(), false)
+            .sorted(comparing((Statusreport sr) -> sr.getCustomerorder().getSign())
+                .thenComparing(Statusreport::getSort)
+                .thenComparing(Statusreport::getFromdate)
+                .thenComparing(Statusreport::getUntildate)
+                .thenComparing(sr -> sr.getSender().getSign()))
+            .collect(Collectors.toList());
     }
 
     /**
      * Get a list of all visible {@link Statusreport}s.
      */
-    @SuppressWarnings("unchecked")
     public List<Statusreport> getVisibleStatusReports() {
-        Date now = new Date();
-        return getSession().createQuery("from Statusreport sr " +
-                "where sr.customerorder.hide is null " +
-                "or sr.customerorder.hide = false " +
-                "or (sr.customerorder.fromDate <= ? and (sr.customerorder.untilDate = null or sr.customerorder.untilDate >= ? )) " +
-                "order by sr.customerorder.sign asc, sr.sort asc, sr.fromdate asc, sr.untildate asc, sr.sender.sign asc")
-                .setDate(0, now).setDate(1, now).list();
+        final Date now = DateUtils.now();
+        return StreamSupport.stream(statusreportRepository.findAll().spliterator(), false)
+            .filter(sr -> !TRUE.equals(sr.getCustomerorder().getHide()))
+            .filter(sr -> !sr.getCustomerorder().isValidAt(now))
+            .sorted(comparing((Statusreport sr) -> sr.getCustomerorder().getSign())
+                .thenComparing(Statusreport::getSort)
+                .thenComparing(Statusreport::getFromdate)
+                .thenComparing(Statusreport::getUntildate)
+                .thenComparing(sr -> sr.getSender().getSign()))
+            .collect(Collectors.toList());
     }
 
     /**
      * Get a list of all {@link Statusreport}s associated with the given {@link Customerorder}.
      */
-    @SuppressWarnings("unchecked")
     public List<Statusreport> getStatusReportsByCustomerOrderId(long coId) {
-        return getSession().createQuery("from Statusreport sr where customerorder.id = ? " +
-                "order by sr.customerorder.sign asc, sr.sort asc, sr.fromdate asc, sr.untildate asc, sr.sender.sign asc")
-                .setLong(0, coId)
-                .list();
+        return statusreportRepository.findAllByCustomerorderId(coId).stream()
+            .sorted(comparing((Statusreport sr) -> sr.getCustomerorder().getSign())
+                .thenComparing(Statusreport::getSort)
+                .thenComparing(Statusreport::getFromdate)
+                .thenComparing(Statusreport::getUntildate)
+                .thenComparing(sr -> sr.getSender().getSign()))
+            .collect(Collectors.toList());
     }
 
     /**
@@ -115,23 +120,7 @@ public class StatusReportDAO extends AbstractDAO {
      * @param loginEmployee The login employee
      */
     public void save(Statusreport sr, Employee loginEmployee) {
-        if (loginEmployee == null) {
-            throw new RuntimeException("the login-user must be passed to the db");
-        }
-        Session session = getSession();
-        java.util.Date creationDate = sr.getCreated();
-        if (creationDate == null) {
-            sr.setCreated(new java.util.Date());
-            sr.setCreatedby(loginEmployee.getSign());
-        } else {
-            sr.setLastupdate(new java.util.Date());
-            sr.setLastupdatedby(loginEmployee.getSign());
-            Integer updateCounter = sr.getUpdatecounter();
-            updateCounter = (updateCounter == null) ? 1 : updateCounter + 1;
-            sr.setUpdatecounter(updateCounter);
-        }
-        session.saveOrUpdate(sr);
-        session.flush();
+        statusreportRepository.save(sr);
     }
 
     /**
@@ -141,23 +130,8 @@ public class StatusReportDAO extends AbstractDAO {
      * @return boolean
      */
     public boolean deleteStatusReportById(long srId) {
-        Statusreport srToDelete = getStatusReportById(srId);
-        boolean srDeleted = false;
-
-        // check if related status report still exists
-
-        if (srToDelete != null) {
-            Session session = getSession();
-            session.delete(srToDelete);
-            session.flush();
-            srDeleted = true;
-        }
-        return srDeleted;
-    }
-
-    public List<Statusreport> getStatusReportByEmployeeorderId(long employeeorderId) {
-        // FIXME implement
-        return Collections.emptyList();
+        statusreportRepository.deleteById(srId);
+        return true;
     }
 
 }
