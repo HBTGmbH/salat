c6df4f03cebc9cac01965f82b66f762563ce6b24
Klaus Richarz <kr@hbt.de>
2022-02-20 22:42:30 +0100

    [#44] Fix SuborderDAO functionality

--- src/main/java/org/tb/persistence/SuborderDAO.java
+++ src/main/java/org/tb/persistence/SuborderDAO.java
@@ -1,69 +1,64 @@
 package org.tb.persistence;
 
+import static java.lang.Boolean.TRUE;
+import static java.util.Comparator.comparing;
+import static org.springframework.data.domain.Sort.Direction.ASC;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import org.apache.commons.lang.StringUtils;
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+import javax.persistence.criteria.Predicate;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
-import org.tb.bdom.*;
+import org.tb.bdom.Customerorder_;
+import org.tb.bdom.Employee;
+import org.tb.bdom.Employeeorder;
+import org.tb.bdom.Suborder;
+import org.tb.bdom.Suborder_;
 import org.tb.bdom.comparators.SubOrderComparator;
-
-import java.util.Map.Entry;
 import org.tb.util.DateUtils;
 
 @Component
-public class SuborderDAO extends AbstractDAO {
+@RequiredArgsConstructor
+public class SuborderDAO {
     private static final Logger LOG = LoggerFactory.getLogger(SuborderDAO.class);
 
     private final SuborderRepository suborderRepository;
-
-    @Autowired
-    public SuborderDAO(SessionFactory sessionFactory, SuborderRepository suborderRepository) {
-        super(sessionFactory);
-        this.suborderRepository = suborderRepository;
-    }
 
     /**
      * Gets the suborder for the given id.
      */
     public Suborder getSuborderById(long id) {
-        return (Suborder) getSession().get(Suborder.class, id);
-    }
-
-    public Suborder getSuborderBySign(String sign) {
-        return (Suborder) getSession().createQuery("from Suborder so where so.sign = ?").setString(0, sign).uniqueResult();
+        return suborderRepository.findById(id).orElse(null);
     }
 
     /**
      * Gets a list of Suborders by employee contract id.
      */
     public List<Suborder> getSubordersByEmployeeContractId(long contractId) {
-        @SuppressWarnings("unchecked")
-        List<Suborder> suborders = getSession()
-                .createQuery("select e.suborder from Employeeorder e where e.employeecontract.id = ? order by e.suborder.sign asc, e.suborder.description asc")
-                .setLong(0, contractId)
-                .list();
-        return suborders;
+        return suborderRepository.findAllByEmployeecontractId(contractId);
     }
 
     /**
      * Gets a list of Suborders by employee contract id AND customerorder.
      */
-    public List<Suborder> getSubordersByEmployeeContractIdAndCustomerorderId(long contractId, long coId, boolean onlyValid) {
-        List<Suborder> employeeSpecificSuborders = getSubordersByEmployeeContractId(contractId);
+    public List<Suborder> getSubordersByEmployeeContractIdAndCustomerorderId(long employeeContractId, long customerorderId, boolean onlyValid) {
+        List<Suborder> employeeSpecificSuborders = getSubordersByEmployeeContractId(employeeContractId);
         if (!onlyValid) return employeeSpecificSuborders;
 
-        List<Suborder> allSuborders = new ArrayList<Suborder>();
+        var allSuborders = new ArrayList<Suborder>();
         for (Suborder so : employeeSpecificSuborders) {
-            if (so.getCustomerorder().getId() == coId) {
+            if (so.getCustomerorder().getId().equals(customerorderId)) {
                 if (so.getCurrentlyValid()) {
                     allSuborders.add(so);
                 }
@@ -78,40 +73,16 @@ public class SuborderDAO extends AbstractDAO {
      * Gets all {@link Suborder}s for the given employee, restricted to those that have
      * valid {@link Employeeorder}s.
      *
-     * @param ecId id of the employee's contract
+     * @param employeecontractId id of the employee's contract
      * @param date the date to check validity against
      * @return a distinct list of matching {@link Suborder}s
      */
-    @SuppressWarnings("unchecked")
-    public List<Suborder> getSubordersByEmployeeContractIdWithValidEmployeeOrders(long ecId, Date date) {
-        Session session = getSession();
-        return session.createQuery("select distinct so from Employeeorder eo inner join eo.suborder so inner join so.customerorder co " +
-                "where " +
-                "eo.employeecontract = :employeecontract " +
-                "and eo.fromDate <= :refDate " +
-                "and (eo.untilDate is null " +
-                "or eo.untilDate >= :refDate) " +
-                "order by co.sign asc, so.sign asc")
-                .setEntity("employeecontract", session.load(Employeecontract.class, ecId))
-                .setDate("refDate", date)
-                .list();
+    public List<Suborder> getSubordersByEmployeeContractIdWithValidEmployeeOrders(long employeecontractId, Date date) {
+        return suborderRepository.findAllByEmployeecontractIdAndEmployeeorderValidAt(employeecontractId, date);
     }
 
-    @SuppressWarnings("unchecked")
-    public List<Suborder> getSubordersByEmployeeContractIdAndCustomerorderIdWithValidEmployeeOrders(long ecId, long coId, Date date) {
-        Session session = getSession();
-        return session.createQuery("select distinct so from Employeeorder eo inner join eo.suborder so inner join so.customerorder co " +
-                "where " +
-                "eo.employeecontract = :employeecontract " +
-                "and so.customerorder = :customerorder " +
-                "and eo.fromDate <= :refDate " +
-                "and (eo.untilDate is null " +
-                "or eo.untilDate >= :refDate) " +
-                "order by so.sign asc, so.description")
-                .setEntity("employeecontract", session.load(Employeecontract.class, ecId))
-                .setEntity("customerorder", session.load(Customerorder.class, coId))
-                .setDate("refDate", date)
-                .list();
+    public List<Suborder> getSubordersByEmployeeContractIdAndCustomerorderIdWithValidEmployeeOrders(long employeecontractId, long customerorderId, Date date) {
+        return suborderRepository.findAllByEmployeecontractIdAndCustomerorderIdAndEmployeeorderValidAt(employeecontractId, customerorderId, date);
     }
 
     /**
@@ -119,35 +90,21 @@ public class SuborderDAO extends AbstractDAO {
      */
     public List<Suborder> getSubordersByCustomerorderId(long customerorderId, boolean onlyValid) {
         if (onlyValid) {
-            return getSubordersByCustomerorderId(customerorderId, new java.util.Date());
+            return getSubordersByCustomerorderId(customerorderId, DateUtils.now());
         } else {
-            @SuppressWarnings("unchecked")
-            List<Suborder> result = getSession()
-                    .createQuery("from Suborder s where s.customerorder.id = ? order by sign")
-                    .setLong(0, customerorderId)
-                    .setCacheable(true)
-                    .list();
-            return result;
+            var order = new Order(ASC, Suborder_.SIGN);
+            return suborderRepository.findAllByCustomerorderId(customerorderId, Sort.by(order));
         }
     }
 
     /**
      * Gets a list of Suborders by customer order id.
      */
-    public List<Suborder> getSubordersByCustomerorderId(long customerorderId, java.util.Date date) {
-        @SuppressWarnings("unchecked")
-
-        List<Suborder> result = getSession()
-                .createQuery("from Suborder s where s.customerorder.id = :coId " +
-                        "and s.fromDate <= :refDate " +
-                        "and (s.untilDate is null " +
-                        "or s.untilDate >= :refDate) " +
-                        "order by sign")
-                .setParameter("coId", customerorderId)
-                .setParameter("refDate", date)
-                .setCacheable(true)
-                .list();
-        return result;
+    public List<Suborder> getSubordersByCustomerorderId(long customerorderId, Date date) {
+        var order = new Order(ASC, Suborder_.SIGN);
+        return suborderRepository.findAllByCustomerorderId(customerorderId, Sort.by(order)).stream()
+            .filter(s -> s.isValidAt(date))
+            .collect(Collectors.toList());
     }
 
     /**
@@ -157,115 +114,89 @@ public class SuborderDAO extends AbstractDAO {
      */
     public List<Suborder> getSuborders(boolean onlyValid) {
         if (onlyValid) {
-            return getSuborders(new java.util.Date());
+            return getSuborders(DateUtils.now());
         } else {
-
-            @SuppressWarnings("unchecked")
-            List<Suborder> result = getSession().createQuery("from Suborder order by sign").list();
-
-            return result;
+            return StreamSupport.stream(suborderRepository.findAll().spliterator(), false)
+                .sorted(comparing(Suborder::getSign))
+                .collect(Collectors.toList());
         }
     }
 
     /**
      * Get a list of all Suborders ordered by their sign.
      */
-    public List<Suborder> getSuborders(java.util.Date date) {
-        @SuppressWarnings("unchecked")
-        List<Suborder> result = getSession().createQuery("from Suborder s " +
-                "where s.fromDate <= :refDate " +
-                "and (s.untilDate is null " +
-                "or s.untilDate >= :refDate) " +
-                "order by sign")
-                .setParameter("refDate", date)
-                .list();
-
-        return result;
+    public List<Suborder> getSuborders(Date date) {
+        return StreamSupport.stream(suborderRepository.findAll().spliterator(), false)
+            .filter(s -> s.isValidAt(date))
+            .sorted(comparing(Suborder::getSign))
+            .collect(Collectors.toList());
     }
 
-    private List<Suborder> createSuborderQuery(Long customerOrderId, String filter, Date dateFrom, Date dateTill) {
-        Map<String, Object> args = new HashMap<String, Object>();
-        List<String> terms = new ArrayList<String>();
+    private Specification<Suborder> showOnlyValid() {
+        Date now = DateUtils.now();
+        return (root, query, builder) -> {
+            var fromDateLess = builder.lessThanOrEqualTo(root.get(Suborder_.fromDate), now);
+            var untilDateNullOrGreater = builder.or(
+                builder.isNull(root.get(Suborder_.untilDate)),
+                builder.greaterThanOrEqualTo(root.get(Suborder_.untilDate), now)
+            );
+            var notHidden = builder.notEqual(root.get(Suborder_.hide), TRUE);
+            return builder.and(fromDateLess, untilDateNullOrGreater, notHidden);
+        };
+    }
 
-        if (customerOrderId != null) {
-            terms.add("s.customerorder.id = :custormerOrderId");
-            args.put("custormerOrderId", customerOrderId);
-        }
+    private Specification<Suborder> matchingCustomerorderId(long customerorderId) {
+        return (root, query, builder) -> builder.equal(root.join(Suborder_.customerorder).get(Customerorder_.id), customerorderId);
+    }
 
-        if (filter != null) {
-            terms.add("(upper(sign) like :filter "
-                    + "or upper(description) like :filter "
-                    + "or upper(s.customerorder.sign) like :filter "
-                    + "or upper(s.customerorder.description) like :filter "
-                    + "or upper(shortdescription) like :filter "
-                    + "or upper(s.customerorder.shortdescription) like :filter "
-                    + "or upper(hourly_rate) like :filter)");
-            args.put("filter", filter);
-        }
-
-        if (dateFrom != null) {
-            terms.add("(fromDate <= :fromDate "
-                    + "or (fromDate = null "
-                    + "and s.customerorder.fromDate <= :fromDate ))");
-            args.put("fromDate", dateFrom);
-        }
-
-        if (dateTill != null) {
-            terms.add("(untilDate >= :untilDate "
-                    + "or (untilDate = null "
-                    + "and (s.customerorder.untilDate = null "
-                    + "or s.customerorder.untilDate <= :untilDate )))");
-            args.put("untilDate", dateTill);
-        }
-
-        StringBuilder sb = new StringBuilder();
-        sb.append("from Suborder s ");
-        if (!terms.isEmpty()) {
-            String termsStr = StringUtils.join(terms, " and ");
-            sb.append(" where (");
-            sb.append(termsStr);
-            sb.append(") ");
-        }
-        sb.append(" order by s.customerorder.sign ,sign");
-
-        Query query = getSession().createQuery(sb.toString());
-        for (Entry<String, Object> entry : args.entrySet()) {
-            query = query.setParameter(entry.getKey(), entry.getValue());
-        }
-
-        @SuppressWarnings("unchecked")
-        List<Suborder> suborders = query.list();
-        return suborders;
+    private Specification<Suborder> filterMatches(String filter) {
+        final var filterValue = ('%' + filter + '%').toUpperCase();
+        return (root, query, builder) -> builder.or(
+            builder.like(builder.upper(root.get(Suborder_.description)), filterValue),
+            builder.like(builder.upper(root.get(Suborder_.shortdescription)), filterValue),
+            builder.like(builder.upper(root.get(Suborder_.sign)), filterValue),
+            builder.like(builder.upper(root.join(Suborder_.customerorder).get(Customerorder_.sign)), filterValue),
+            builder.like(builder.upper(root.join(Suborder_.customerorder).get(Customerorder_.description)), filterValue),
+            builder.like(builder.upper(root.join(Suborder_.customerorder).get(Customerorder_.shortdescription)), filterValue)
+        );
     }
 
     /**
      * Get a list of all suborders fitting to the given filters ordered by their sign.
      */
-    public List<Suborder> getSubordersByFilters(Boolean showInvalid, String filter, Long customerOrderId) {
-        boolean isFilter = filter != null && !filter.trim().isEmpty();
-        if (isFilter) {
-            filter = "%" + filter.toUpperCase() + "%";
-        } else {
-            filter = null;
-        }
-        if (customerOrderId != null && customerOrderId == -1) customerOrderId = null;
-
-        Date now = showInvalid == null || !showInvalid ? new Date() : null;
-        return createSuborderQuery(customerOrderId, filter, now, now);
+    public List<Suborder> getSubordersByFilters(Boolean showInvalid, String filter, Long customerorderId) {
+        var order = new Order(ASC, Suborder_.SIGN);
+        return suborderRepository.findAll((root, query, builder) -> {
+            Set<Predicate> predicates = new HashSet<>();
+            if(!TRUE.equals(showInvalid)) {
+                predicates.add(showOnlyValid().toPredicate(root, query, builder));
+            }
+            if(customerorderId != null && customerorderId > 0) {
+                predicates.add(matchingCustomerorderId(customerorderId).toPredicate(root, query, builder));
+            }
+            boolean isFilter = filter != null && !filter.trim().isEmpty();
+            if(isFilter) {
+                predicates.add(filterMatches(filter).toPredicate(root, query, builder));
+            }
+            return builder.and(predicates.toArray(new Predicate[0]));
+        }, Sort.by(order));
     }
 
     /**
      * Get a list of all children of the suborder associated to the given soId ordered by their sign.
      */
-    @SuppressWarnings("unchecked")
     public List<Suborder> getSuborderChildren(long soId) {
-        return getSession().createQuery("from Suborder so where so.parentorder.id = ? order by sign").setLong(0, soId).list();
+        return suborderRepository.findById(soId)
+            .map(Suborder::getSuborders)
+            .orElse(Collections.emptyList())
+            .stream()
+            .sorted(comparing(Suborder::getSign))
+            .collect(Collectors.toList());
     }
 
     /**
      * @return Returns all {@link Suborder}s where the standard flag is true and that did not end before today.
      */
-    @SuppressWarnings("unchecked")
     public List<Suborder> getStandardSuborders() {
         return suborderRepository.findAllStandardSubordersByUntilDateGreaterThanEqual(DateUtils.today());
     }
@@ -281,47 +212,22 @@ public class SuborderDAO extends AbstractDAO {
      * Saves the given suborderand sets creation-/update-user and creation-/update-date.
      */
     public void save(Suborder so, Employee loginEmployee) {
-        if (loginEmployee == null) {
-            throw new RuntimeException("the login-user must be passed to the db");
-        }
-        Session session = getSession();
-        java.util.Date creationDate = so.getCreated();
-        if (creationDate == null) {
-            so.setCreated(new java.util.Date());
-            so.setCreatedby(loginEmployee.getSign());
-        } else {
-            so.setLastupdate(new java.util.Date());
-            so.setLastupdatedby(loginEmployee.getSign());
-            Integer updateCounter = so.getUpdatecounter();
-            updateCounter = updateCounter == null ? 1 : updateCounter + 1;
-            so.setUpdatecounter(updateCounter);
-        }
-        session.saveOrUpdate(so);
-        session.flush();
-        //session.clear(); nicht praktikabel da später benötigte objekte der session bekannt aber nicht mehr vorhanden sind. bei speicherproblemen evtl. session.evict()
+        suborderRepository.save(so);
     }
 
     /**
      * Deletes the given suborder.
      */
     public boolean deleteSuborderById(long soId) {
-        Suborder soToDelete = getSuborderById(soId);
-        if (soToDelete == null) {
-            return false;
-        }
-
-        // check if related timereports, employee orders, suborders exist - if so, no deletion possible
-        if (soToDelete.getEmployeeorders() != null && !soToDelete.getEmployeeorders().isEmpty()) return false;
-
-        if (soToDelete.getTimereports() != null && !soToDelete.getTimereports().isEmpty()) return false;
-
-        if (soToDelete.getSuborders() != null && !soToDelete.getSuborders().isEmpty()) return false;
-
-        Session session = getSession();
-        session.delete(soToDelete);
-        session.flush();
-        LOG.debug("SuborderDAO.deleteSuborderById - deleted object {} and flushed!", soToDelete);
-        return true;
+        return suborderRepository.findById(soId).map(suborder -> {
+            // check if related timereports, employee orders, suborders exist - if so, no deletion possible
+            if (suborder.getEmployeeorders() != null && !suborder.getEmployeeorders().isEmpty()) return false;
+            if (suborder.getTimereports() != null && !suborder.getTimereports().isEmpty()) return false;
+            if (suborder.getSuborders() != null && !suborder.getSuborders().isEmpty()) return false;
+            suborderRepository.delete(suborder);
+            LOG.debug("SuborderDAO.deleteSuborderById - deleted object {}", suborder);
+            return true;
+        }).orElse(false);
     }
 
 }
