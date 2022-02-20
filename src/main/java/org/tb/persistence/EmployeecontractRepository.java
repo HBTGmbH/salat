6af9925945d9e404c88b22ae232e2709dba5e33d
Klaus Richarz <kr@hbt.de>
2022-02-20 18:51:52 +0100

    [#44] Fix EmployeecontractDAO functionality

--- src/main/java/org/tb/persistence/EmployeecontractRepository.java
+++ src/main/java/org/tb/persistence/EmployeecontractRepository.java
@@ -3,13 +3,15 @@ package org.tb.persistence;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import org.tb.bdom.Employeecontract;
 
 @Repository
-public interface EmployeecontractRepository extends CrudRepository<Employeecontract, Long> {
+public interface EmployeecontractRepository extends PagingAndSortingRepository<Employeecontract, Long>,
+    JpaSpecificationExecutor<Employeecontract> {
 
   @Query("select e from Employeecontract e where e.employee.id = :employeeId and e.validFrom <= :validAt and (e.validUntil >= :validAt or e.validUntil is null)")
   Optional<Employeecontract> findByEmployeeIdAndValidAt(long employeeId, Date validAt);
@@ -17,4 +19,12 @@ public interface EmployeecontractRepository extends CrudRepository<Employeecontr
   @Query("select e from Employeecontract e where (e.hide is false or e.hide is null) "
       + "and (e.validFrom <= :date and (e.validUntil >= :date or e.validUntil is null))")
   List<Employeecontract> findAllValidAtAndNotHidden(Date date);
+
+  @Query("""
+    select ec from Employeecontract ec
+    where ec.supervisor.id = :supervisorId and ec.validFrom <= :date and (ec.validUntil is null or ec.validUntil >= :date)
+    order by ec.employee.lastname asc, ec.validFrom asc
+  """)
+  List<Employeecontract> findAllSupervisedValidAt(long supervisorId, Date date);
+
 }
