f533838780cd24796a71e307613ae1f90f25b69c
Klaus Richarz <kr@hbt.de>
2022-02-20 20:41:18 +0100

    [#44] Fix OvertimeDAO functionality

--- src/main/java/org/tb/persistence/OvertimeRepository.java
+++ src/main/java/org/tb/persistence/OvertimeRepository.java
@@ -1,12 +1,12 @@
 package org.tb.persistence;
 
 import java.util.List;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import org.tb.bdom.Overtime;
 
 @Repository
-public interface OvertimeRepository extends CrudRepository<Overtime, Long> {
+public interface OvertimeRepository extends PagingAndSortingRepository<Overtime, Long> {
 
   List<Overtime> findAllByEmployeecontractId(long employeeContractId);
 
