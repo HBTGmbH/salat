72ce2d70bbed19649515a526902af3ee4e07e14c
Klaus Richarz <kr@hbt.de>
2022-03-18 11:42:23 +0100

    Fix method names to follow underscores

--- src/test/java/org/tb/common/util/DateUtilsTest.java
+++ src/test/java/org/tb/common/util/DateUtilsTest.java
@@ -4,26 +4,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
 
+@DisplayNameGeneration(ReplaceUnderscores.class)
 class DateUtilsTest {
 
   @Test
-  void sameDatesMustReturnWorkingDayDistance1IfWorkingDay() {
+  void same_dates_must_return_working_day_distance1_if_working_day() {
     LocalDate a = LocalDate.of(2022, 3, 1);
     LocalDate b = LocalDate.of(2022, 3, 1);
     assertThat(DateUtils.getWorkingDayDistance(a, b)).isEqualTo(1);
   }
 
   @Test
-  void sameDatesMustReturnWorkingDayDistance0IfNotWorkingDay() {
+  void same_dates_must_return_working_day_distance0_if_not_working_day() {
     LocalDate a = LocalDate.of(2022, 3, 5);
     LocalDate b = LocalDate.of(2022, 3, 5);
     assertThat(DateUtils.getWorkingDayDistance(a, b)).isEqualTo(0);
   }
 
   @Test
-  void workingDayDistanceMustConsiderWeekends() {
+  void working_day_distance_must_consider_weekends() {
     LocalDate a = LocalDate.of(2022, 3, 1);
     LocalDate b = LocalDate.of(2022, 3, 11);
     assertThat(DateUtils.getWorkingDayDistance(a, b)).isEqualTo(9);
