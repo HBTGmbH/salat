636baeba7963e1b1dc1688f155dced1085b3ca4c
Klaus Richarz <kr@hbt.de>
2022-03-19 01:31:07 +0100

    Remove unnecessary save of employee

--- src/main/java/org/tb/auth/LogoutEmployeeAction.java
+++ src/main/java/org/tb/auth/LogoutEmployeeAction.java
@@ -8,8 +8,6 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.springframework.stereotype.Component;
 import org.tb.common.struts.LoginRequiredAction;
-import org.tb.employee.domain.Employee;
-import org.tb.employee.persistence.EmployeeDAO;
 
 /**
  * Action class for the logout of an employee
@@ -20,15 +18,8 @@ import org.tb.employee.persistence.EmployeeDAO;
 @RequiredArgsConstructor
 public class LogoutEmployeeAction extends LoginRequiredAction<ActionForm> {
 
-    private final EmployeeDAO employeeDAO;
-
     @Override
     protected ActionForward executeAuthenticated(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        Employee loginEmployee = (Employee) request.getSession().getAttribute("loginEmployee");
-        if (loginEmployee != null) {
-            employeeDAO.save(loginEmployee, loginEmployee);
-        }
-
         request.getSession().removeAttribute("currentEmployee");
         request.getSession().removeAttribute("currentOrder");
         request.getSession().removeAttribute("employees");
