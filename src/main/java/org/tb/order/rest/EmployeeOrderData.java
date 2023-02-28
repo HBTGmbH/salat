5037f19c5d82cbe9a54fa96eb255643786a57d57
Josias Polchau <jp@hbt.de>
2023-02-28 14:17:04 +0100

    hierarchic orders endpoint

--- src/main/java/org/tb/order/rest/EmployeeOrderData.java
+++ src/main/java/org/tb/order/rest/EmployeeOrderData.java
@@ -1,9 +1,11 @@
 package org.tb.order.rest;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
+@Builder
 @RequiredArgsConstructor
 public class EmployeeOrderData {
     private final SuborderData suborder;
