cd9efa5461a13e57bff9bd55d5e92b251df784f9
Kai Dittmer <kai.dittmer@hbt.de>
2023-11-22 10:56:10 +0100

    add cors handling for /rest/**

--- src/main/java/org/tb/auth/OAuth2LoginSecurityConfig.java
+++ src/main/java/org/tb/auth/OAuth2LoginSecurityConfig.java
@@ -3,67 +3,68 @@ package org.tb.auth;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 import java.util.List;
-import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.Ordered;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @Profile({"!e2etest"})
+@Slf4j
 public class OAuth2LoginSecurityConfig{
 
-  public final static List<String> EXCLUDE_PATTERN = List.of(
+  public final static String[] EXCLUDE_PATTERN = List.of(
       "/favicon.ico",
       "/error",
       "/**error**",
       "/v3/api-docs",
       "/actuator/*",
-      "/error.jsp");
-  private final List<AntPathRequestMatcher> excludePattern = EXCLUDE_PATTERN.stream()
-      .map(s -> new AntPathRequestMatcher(s, null)).toList();
+      "/error.jsp")
+      .toArray(new String[0]);
 
+  private final CorsRestConfiguration corsRestConfiguration;
 
   /**
    * Add configuration logic as needed.
    */
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    return configRest(http)
+    http = configRest(http);
+    http = configLegacySalat(http);
+    return http.build();
+  }
+
+  private HttpSecurity configLegacySalat(HttpSecurity http) throws Exception {
+    return http
         .authorizeHttpRequests(
             (authorize) -> authorize
-                .antMatchers(EXCLUDE_PATTERN.toArray(new String[0]))
+                .antMatchers(EXCLUDE_PATTERN)
                 .permitAll()
                 .antMatchers("/**")
-                //.hasRole("salat-user")
-                //.anyRequest()
                 .authenticated()
         )
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS).and()
         .oauth2Login(withDefaults())
-        .csrf().disable()
-        .cors().configurationSource(corsConfigurationSource()).and()
-//        .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-        .build();
+        .csrf().disable();
   }
 
   private HttpSecurity configRest(HttpSecurity httpSecurity) throws Exception {
     return httpSecurity
-
-        //.addFilterAfter(hbtAuthenticationFilter, )
         .authorizeHttpRequests(
             (authorize) -> authorize
-                .antMatchers(EXCLUDE_PATTERN.toArray(new String[0]))
+                .antMatchers(EXCLUDE_PATTERN)
                 .permitAll()
                 .antMatchers("/rest/**")
                 .hasAuthority("SCOPE_user_impersonation")
@@ -71,22 +72,30 @@ public class OAuth2LoginSecurityConfig{
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER).and()
         .oauth2ResourceServer(oauth2 -> oauth2.jwt())
         .csrf().disable()
-        .cors().configurationSource(corsConfigurationSource()).and();
+        ;
   }
 
-  private CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration configuration = new CorsConfiguration();
-    configuration.applyPermitDefaultValues();
-    configuration.addAllowedOrigin("*");
-    configuration.addAllowedHeader("*");
-    configuration.addAllowedMethod("*");
-    configuration.setAllowCredentials(true);
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/rest/**", configuration);
-    return source;
+  @Bean
+  public FilterRegistrationBean corsRestFilterBean() {
+    final CorsConfiguration config = new CorsConfiguration();
+
+    config.setAllowCredentials(true);
+
+    if (corsRestConfiguration.getAllowedOrigins() != null) {
+      corsRestConfiguration.getAllowedOrigins().forEach(config::addAllowedOrigin);
+    } else {
+      log.error("property salat.rest.cors.allowed-origins not set. CORS won't work!");
+    }
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("*");
+
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/rest/**", config);
+
+    FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+    bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+    return bean;
   }
 
-  protected boolean shouldNotFilter(HttpServletRequest request) {
-    return excludePattern.stream().anyMatch(matcher -> matcher.matches(request));
-  }
 }
