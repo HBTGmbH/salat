ec434c71a70a9392d0ff57b0566428f6078a0e52
ljhbt <lj@hbt.de>
2023-11-22 16:16:20 +0100

    endpoint create favorite

--- src/main/java/de/hbt/salat/rest/favorites/adapter_rest/FavoriteRestEndpoint.java
+++ src/main/java/de/hbt/salat/rest/favorites/adapter_rest/FavoriteRestEndpoint.java
@@ -1,23 +1,21 @@
 package de.hbt.salat.rest.favorites.adapter_rest;
 
-import static org.springframework.http.HttpStatus.UNAUTHORIZED;
-
 import de.hbt.salat.rest.favorites.core.FavoriteService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.Collection;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.mapstruct.factory.Mappers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.tb.auth.AuthorizedUser;
+
+import java.util.Collection;
+
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @Slf4j
 @RestController
@@ -39,6 +37,12 @@ public class FavoriteRestEndpoint {
       throw new ResponseStatusException(UNAUTHORIZED);
     }
     return favoriteDtoMapper.map(favoriteService.getFavorites());
+  }
+
+  @PostMapping(path = "", consumes = MediaType.APPLICATION_JSON_VALUE)
+  @ResponseStatus(HttpStatus.CREATED)
+  @Operation(security = {@SecurityRequirement(name = "bearerAuth")})
+  public void addFavorite(@RequestBody FavoriteDto favorite) {
   }
 }
 
