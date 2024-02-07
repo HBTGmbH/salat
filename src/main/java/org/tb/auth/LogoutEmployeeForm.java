b16a4e26080fccc69d7f84a1e88f3c473fdb6739
Klaus Richarz <kr@hbt.de>
2024-02-07 19:13:26 +0100

    #294 remove login/logout actions, forms and views

--- src/main/java/org/tb/auth/LogoutEmployeeForm.java
+++
@@ -1,7 +0,0 @@
-package org.tb.auth;
-
-import org.apache.struts.action.ActionForm;
-
-public class LogoutEmployeeForm extends ActionForm {
-    private static final long serialVersionUID = 1L; // 7096994213160642196L;
-}
