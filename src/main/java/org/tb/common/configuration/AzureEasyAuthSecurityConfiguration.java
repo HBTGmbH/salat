511aaff8ffa58793001b3a1d52b1d56ed63c74b0
Klaus Richarz <kr@hbt.de>
2024-02-08 14:54:46 +0100

    #294 add test profile

--- src/main/java/org/tb/common/configuration/AzureEasyAuthSecurityConfiguration.java
+++ src/main/java/org/tb/common/configuration/AzureEasyAuthSecurityConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.security.oauth2.server.resource.web.HeaderBearerToken
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@Profile("production")
+@Profile({ "production", "test" })
 public class AzureEasyAuthSecurityConfiguration {
 
   @Bean
