e8e60eded337bd5db74b453670adb6ab62c3e818
Josias Polchau <jp@hbt.de>
2024-02-12 11:52:38 +0100

    #198 cleanup FieldDefaults

--- src/main/java/org/tb/favorites/rest/FavoriteDto.java
+++ src/main/java/org/tb/favorites/rest/FavoriteDto.java
@@ -1,18 +1,15 @@
 package org.tb.favorites.rest;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.experimental.FieldDefaults;
 
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@FieldDefaults(level = AccessLevel.PRIVATE)
 @Schema(description = "to save your favorite reports", name = "Favorite")
 public class FavoriteDto {
   private Long id;
