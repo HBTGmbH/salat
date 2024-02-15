daaaadd8b11ba8a490fc1b468c212e8d81d8d971
Klaus Richarz <kr@hbt.de>
2024-02-15 12:54:38 +0100

    #301 organize auth configuration

--- src/main/java/org/tb/common/configuration/SalatProperties.java
+++ src/main/java/org/tb/common/configuration/SalatProperties.java
@@ -1,15 +1,35 @@
 package org.tb.common.configuration;
 
+import java.time.Duration;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
+@Data
 @Configuration
 @ConfigurationProperties(prefix = "salat")
-@Data
 public class SalatProperties {
 
   private String url;
   private String mailHost;
+  private Auth auth;
+  private AuthService authService;
+
+  @Data
+  public static class Auth {
+    private OidcIdToken oidcIdToken;
+
+    @Data
+    public static class OidcIdToken {
+      private String principalClaimName;
+      private String headerName;
+    }
+  }
+
+  @Data
+  public static class AuthService {
+    private Duration cacheExpiry;
+  }
 
 }
