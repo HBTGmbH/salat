d9b18772482711b50dc9654cdd1e0b4a2a5508fe
Klaus Richarz <kr@hbt.de>
2024-02-15 12:19:21 +0100

    #301 use oidc id token instead of azure prop easyauth json data

--- src/main/java/org/tb/common/configuration/AzureEasyAuthSecurityConfiguration.java
+++ src/main/java/org/tb/common/configuration/AzureEasyAuthSecurityConfiguration.java
@@ -1,91 +1,50 @@
 package org.tb.common.configuration;
 
-import com.jayway.jsonpath.JsonPath;
-import java.nio.charset.Charset;
-import java.util.Base64;
-import java.util.List;
-import java.util.Set;
-import javax.servlet.http.HttpServletRequest;
-import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
-import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenResolver;
+import org.springframework.security.oauth2.server.resource.web.HeaderBearerTokenResolver;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
-import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider;
-import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
-import org.tb.employee.domain.Employee;
 
 @Configuration
 @Profile({ "production", "test" })
 public class AzureEasyAuthSecurityConfiguration {
 
+  @Value("${}")
+  private String idTokenHeaderName;
+
+  @Value("${}")
+  private String idTokenPrinciplaClaimName;
+
   @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
-    http.addFilter(preAuthenticatedProcessingFilter(authenticationManager)).authorizeRequests(authz -> authz.antMatchers("/do/**", "**/*.jsp", "/rest/**").authenticated());
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.authorizeRequests(authz -> authz.antMatchers("/do/**", "**/*.jsp", "/rest/**").authenticated())
+        .oauth2ResourceServer(oauth2 -> oauth2.jwt());
     http.csrf().disable();
     return http.build();
   }
 
   @Bean
   public WebSecurityCustomizer ignoringCustomizer() {
-    return (web) -> web.ignoring().antMatchers("*.png", "/images/**", "/style/**", "/scripts/**", "/webjars/**", "/favicon.ico");
+    return (web) -> web.ignoring().antMatchers("*.png", "/images/**", "/style/**", "/scripts/**", "/webjars/**", "/favicon.ico", "/rest/doc/**");
   }
 
   @Bean
-  public AuthenticationManager authenticationManager(AuthenticationProvider authenticationProvider, ApplicationEventPublisher publisher) {
-    ProviderManager providerManager = new ProviderManager(authenticationProvider);
-    providerManager.setAuthenticationEventPublisher(new DefaultAuthenticationEventPublisher(publisher));
-    return providerManager;
-  }
-
-  private AbstractPreAuthenticatedProcessingFilter preAuthenticatedProcessingFilter(AuthenticationManager authenticationManager) {
-    AbstractPreAuthenticatedProcessingFilter preAuthenticatedProcessingFilter = new AbstractPreAuthenticatedProcessingFilter() {
-      @Override
-      protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
-        String claimsBase64 = request.getHeader("x-ms-client-principal");
-        String claims = new String(Base64.getDecoder().decode(claimsBase64.getBytes()), Charset.forName("UTF-8"));
-        List<String> employeeSigns = JsonPath.read(claims, "$.claims[?(@.typ=='mailnickname')].val");
-        String employeeSign = employeeSigns.get(0);
-        if(employeeSign == null) {
-          Employee loginEmployee = (Employee) request.getSession().getAttribute("loginEmployee");
-          if(loginEmployee != null) {
-            employeeSign = loginEmployee.getSign();
-          }
-        }
-        return employeeSign;
-      }
-
-      @Override
-      protected Object getPreAuthenticatedCredentials(HttpServletRequest request) {
-        return "N/A";
-      }
-    };
-    preAuthenticatedProcessingFilter.setAuthenticationManager(authenticationManager);
-    preAuthenticatedProcessingFilter.setCheckForPrincipalChanges(true);
-    return preAuthenticatedProcessingFilter;
+  BearerTokenResolver bearerTokenResolver() {
+    HeaderBearerTokenResolver bearerTokenResolver = new HeaderBearerTokenResolver(idTokenHeaderName);
+    return bearerTokenResolver;
   }
 
   @Bean
-  public AuthenticationProvider authenticationProvider() {
-    PreAuthenticatedAuthenticationProvider provider = new PreAuthenticatedAuthenticationProvider();
-    provider.setPreAuthenticatedUserDetailsService(new AuthenticationUserDetailsService<PreAuthenticatedAuthenticationToken>() {
-      @Override
-      public UserDetails loadUserDetails(PreAuthenticatedAuthenticationToken token) throws UsernameNotFoundException {
-        return new User(token.getName(), "N/A", Set.of());
-      }
-    });
-    return provider;
+  public JwtAuthenticationConverter customJwtAuthenticationConverter() {
+    JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+    converter.setPrincipalClaimName(idTokenPrinciplaClaimName);
+    return converter;
   }
 
 }
