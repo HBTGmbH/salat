53c0d8913082a323c8dd95d7be99543f318fe089
Klaus Richarz <kr@hbt.de>
2024-02-19 14:42:12 +0100

    #305 Improve swagger test UI integration with oauth2

--- src/main/java/org/tb/common/configuration/AzureEasyAuthSecurityConfiguration.java
+++ src/main/java/org/tb/common/configuration/AzureEasyAuthSecurityConfiguration.java
@@ -5,17 +5,19 @@ import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.util.Arrays;
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.web.BearerTokenResolver;
-import org.springframework.security.oauth2.server.resource.web.HeaderBearerTokenResolver;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.util.Assert;
 
 @Configuration
 @Profile({ "production", "test" })
@@ -37,6 +39,31 @@ public class AzureEasyAuthSecurityConfiguration {
   };
 
   @Bean
+  @Order(0)
+  SecurityFilterChain resources(HttpSecurity http) throws Exception {
+    http.requestMatchers((matchers) -> matchers.antMatchers(UNAUTHENTICATED_URL_PATTERNS))
+        .authorizeHttpRequests((authorize) -> authorize.anyRequest().permitAll())
+        .requestCache().disable()
+        .securityContext().disable()
+        .sessionManagement().disable()
+        .csrf().disable();
+    return http.build();
+  }
+
+  @Bean
+  @Order(1)
+  SecurityFilterChain restApi(HttpSecurity http) throws Exception {
+    http.requestMatchers((matchers) -> matchers.antMatchers("/rest/**"))
+        .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+        .oauth2ResourceServer(oauth2 -> oauth2.jwt())
+        .requestCache().disable()
+        .sessionManagement().disable()
+        .csrf().disable();
+    return http.build();
+  }
+
+  @Bean
+  @Order(2)
   public SecurityFilterChain filterChain(HttpSecurity http, SalatProperties salatProperties) throws Exception {
     http.authorizeRequests(authz -> authz.anyRequest().authenticated())
         .oauth2ResourceServer(oauth2 -> oauth2.jwt())
@@ -78,20 +105,11 @@ public class AzureEasyAuthSecurityConfiguration {
   }
 
   @Bean
-  @Order(0)
-  SecurityFilterChain resources(HttpSecurity http) throws Exception {
-    http.requestMatchers((matchers) -> matchers.antMatchers(UNAUTHENTICATED_URL_PATTERNS))
-        .authorizeHttpRequests((authorize) -> authorize.anyRequest().permitAll())
-        .requestCache().disable()
-        .securityContext().disable()
-        .sessionManagement().disable()
-        .csrf().disable();
-    return http.build();
-  }
-
-  @Bean
   BearerTokenResolver bearerTokenResolver(SalatProperties salatProperties) {
-    return new HeaderBearerTokenResolver(salatProperties.getAuth().getOidcIdToken().getHeaderName());
+    return new MultiHeaderBearerTokenResolver(
+        salatProperties.getAuth().getOidcIdToken().getHeaderName(),
+        "Authorization"
+    );
   }
 
   @Bean
@@ -99,6 +117,33 @@ public class AzureEasyAuthSecurityConfiguration {
     JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
     converter.setPrincipalClaimName(salatProperties.getAuth().getOidcIdToken().getPrincipalClaimName());
     return converter;
+  }
+
+  public static class MultiHeaderBearerTokenResolver implements BearerTokenResolver {
+
+    private String[] headers;
+
+    public MultiHeaderBearerTokenResolver(String... headers) {
+      this.headers = headers;
+      for(String header : headers) {
+        Assert.hasText(header, "header cannot be empty");
+      }
+    }
+
+    @Override
+    public String resolve(HttpServletRequest request) {
+      for(String header : headers) {
+        String value = request.getHeader(header);
+        if(value != null) {
+          if(value.startsWith("Bearer ")) {
+            value = value.substring("Bearer ".length());
+          }
+          return value;
+        }
+      }
+      return null;
+    }
+
   }
 
 }
