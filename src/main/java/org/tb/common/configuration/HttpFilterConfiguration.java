966ecea0e7b99203f7bf5dcb8bf64000eda68eed
Klaus Richarz <kr@hbt.de>
2024-02-26 17:52:38 +0100

    Fix /rest api endpoint

--- src/main/java/org/tb/common/configuration/HttpFilterConfiguration.java
+++ src/main/java/org/tb/common/configuration/HttpFilterConfiguration.java
@@ -46,7 +46,7 @@ public class HttpFilterConfiguration {
         var registrationBean = new FilterRegistrationBean<PerformanceLoggingFilter>();
         registrationBean.setOrder(99);
         registrationBean.setFilter(new PerformanceLoggingFilter());
-        registrationBean.addUrlPatterns("/do/*", "/api/*", "*.jsp");
+        registrationBean.addUrlPatterns("/do/*", "/api/*", "/rest/*", "*.jsp");
         return registrationBean;
     }
 
@@ -55,7 +55,7 @@ public class HttpFilterConfiguration {
         var registrationBean = new FilterRegistrationBean<OpenEntityManagerInViewFilter>();
         registrationBean.setOrder(100);
         registrationBean.setFilter(new OpenEntityManagerInViewFilter());
-        registrationBean.addUrlPatterns("/do/*", "/api/*", "*.jsp");
+        registrationBean.addUrlPatterns("/do/*", "/api/*", "/rest/*", "*.jsp");
         return registrationBean;
     }
 
@@ -64,7 +64,7 @@ public class HttpFilterConfiguration {
         var registrationBean = new FilterRegistrationBean<AuthenticationFilter>();
         registrationBean.setOrder(101);
         registrationBean.setFilter(new AuthenticationFilter(authViewHelper, authorizedUser, employeeRepository));
-        registrationBean.addUrlPatterns("/do/*", "/api/*", "*.jsp");
+        registrationBean.addUrlPatterns("/do/*", "/api/*", "/rest/*", "*.jsp");
         return registrationBean;
     }
 
