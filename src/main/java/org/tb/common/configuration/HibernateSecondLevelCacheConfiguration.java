d95bc96e0a170c977bbd191753300054308db96f
peglow <cpe@hbt.de>
2024-07-18 13:49:15 +0200

    #376 changed the way we set system variables to use in ehcache.xml

--- src/main/java/org/tb/common/configuration/HibernateSecondLevelCacheConfiguration.java
+++ src/main/java/org/tb/common/configuration/HibernateSecondLevelCacheConfiguration.java
@@ -1,18 +1,37 @@
 package org.tb.common.configuration;
 
 import org.hibernate.cache.jcache.ConfigSettings;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.jcache.JCacheCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ResourceLoader;
 
-@Configuration(proxyBeanMethods = false)
-@EnableCaching
+import javax.cache.CacheManager;
+import javax.cache.Caching;
+import javax.cache.spi.CachingProvider;
+import java.io.IOException;
+import java.net.URI;
+
+
+@Configuration
 public class HibernateSecondLevelCacheConfiguration {
 
     @Bean
-    public HibernatePropertiesCustomizer hibernateSecondLevelCacheCustomizer(JCacheCacheManager cacheManager) {
-        return (properties) -> properties.put(ConfigSettings.CACHE_MANAGER, cacheManager.getCacheManager());
+    public HibernatePropertiesCustomizer hibernateSecondLevelCacheCustomizer(ResourceLoader resourceLoader,
+                                                                             @Value("${salat.cache.max-entries}") String maxCacheEntries,
+                                                                             @Value("${salat.cache.expiry-tti}") String expiryTti) {
+        return (properties) -> {
+            try {
+                URI uri = resourceLoader.getResource("classpath:ehcache.xml").getURI();
+                CachingProvider cachingProvider = Caching.getCachingProvider();
+                System.setProperty("EHCACHE_EXPIRY_TTI", expiryTti);
+                System.setProperty("EHCACHE_RESOURCES_HEAP_ENTRIES", maxCacheEntries);
+                CacheManager cacheManager = cachingProvider.getCacheManager(uri, cachingProvider.getDefaultClassLoader());
+                properties.put(ConfigSettings.CACHE_MANAGER, cacheManager);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
     }
 }
