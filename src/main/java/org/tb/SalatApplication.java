b22e4fbcff799824c6e8fed96304bc031b241fe3
peglow <cpe@hbt.de>
2024-07-18 13:49:15 +0200

    #376 changed the way we set system variables to use in ehcache.xml

--- src/main/java/org/tb/SalatApplication.java
+++ src/main/java/org/tb/SalatApplication.java
@@ -19,8 +19,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class SalatApplication {
 
   public static void main(String[] args) {
-    // To use environment variables in the ehcache.xml as replacements
-    System.getenv().forEach(System::setProperty);
     SpringApplication.run(SalatApplication.class, args);
   }
 
