9a1a3ac7a9d469ddc9ab38780fbfef269dbe7977
peglow <cpe@hbt.de>
2024-07-18 10:05:12 +0200

    #376 added caching to improve load times when loading matrix

--- src/main/java/org/tb/dailyreport/persistence/WorkingdayRepository.java
+++ src/main/java/org/tb/dailyreport/persistence/WorkingdayRepository.java
@@ -4,7 +4,10 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+import jakarta.persistence.QueryHint;
+import org.hibernate.jpa.HibernateHints;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.tb.dailyreport.domain.Workingday;
@@ -12,6 +15,11 @@ import org.tb.dailyreport.domain.Workingday;
 @Repository
 public interface WorkingdayRepository extends CrudRepository<Workingday, Long> {
 
+  @QueryHints(value = {
+          @QueryHint(name = HibernateHints.HINT_CACHEABLE, value = "true"),
+          @QueryHint(name = HibernateHints.HINT_CACHE_REGION, value = "WorkingdayRepository.findByRefdayAndEmployeecontractId")
+    }
+  )
   Optional<Workingday> findByRefdayAndEmployeecontractId(LocalDate refday, long employeecontractId);
 
   List<Workingday> findAllByEmployeecontractId(long employeeContractId);
