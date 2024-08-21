1bd287017536a5d91581cd8d8dd9225f0d93bd7e
Klaus Richarz <kr@hbt.de>
2024-08-21 14:45:59 +0200

    #408 fix visibility of uesa time reports, improve overtime compensation reporting

--- src/main/java/org/tb/dailyreport/viewhelper/matrix/Matrix.java
+++ src/main/java/org/tb/dailyreport/viewhelper/matrix/Matrix.java
@@ -4,26 +4,20 @@ import static org.tb.common.util.DurationUtils.format;
 
 import java.time.Duration;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class Matrix {
 
     private final List<MatrixLine> matrixLines;
     private final List<MatrixDayTotal> matrixDayTotals;
     private final Duration totalWorkingTime;
     private final Duration totalWorkingTimeTarget;
-    private final Duration totalOvertimeCompensation;
     private final Duration totalWorkingTimeDiff;
-
-    public Matrix(List<MatrixLine> matrixLines, List<MatrixDayTotal> matrixDayTotals, Duration totalWorkingTime, Duration totalWorkingTimeTarget, Duration totalWorkingTimeDiff, Duration totalOvertimeCompensation) {
-        this.matrixLines = matrixLines;
-        this.matrixDayTotals = matrixDayTotals;
-        this.totalWorkingTime = totalWorkingTime;
-        this.totalWorkingTimeTarget = totalWorkingTimeTarget;
-        this.totalWorkingTimeDiff = totalWorkingTimeDiff;
-        this.totalOvertimeCompensation = totalOvertimeCompensation;
-    }
+    private final Duration totalOvertimeCompensation;
+    private final Duration totalWorkingTimeDiffWithCompensation;
 
     public String getTotalWorkingTimeString() {
         return format(totalWorkingTime);
@@ -39,6 +33,10 @@ public class Matrix {
 
     public String getTotalWorkingTimeDiffString() {
         return format(totalWorkingTimeDiff);
+    }
+
+    public String getTotalWorkingTimeDiffWithCompensationString() {
+        return format(totalWorkingTimeDiffWithCompensation);
     }
 
 }
