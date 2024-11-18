b1b66e9bb2f416799283fa01092edd9f498660c7
Klaus Richarz <kr@hbt.de>
2024-11-18 23:31:22 +0100

    Add ArchCondition to check Entity usage in service methods
    Enhanced the test to include an ArchCondition that checks for Entity usage in the public interface of methods annotated with @Service. This ensures better enforcement of architectural rules by providing detailed feedback on any violations, including file name and line number.

--- src/test/java/org/tb/ArchitectureTest.java
+++ src/test/java/org/tb/ArchitectureTest.java
@@ -9,11 +9,17 @@ import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.sli
 import com.tngtech.archunit.base.DescribedPredicate;
 import com.tngtech.archunit.core.domain.JavaClass;
 import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.domain.JavaMethod;
+import com.tngtech.archunit.core.domain.JavaParameterizedType;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.lang.ArchCondition;
 import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.ConditionEvents;
 import com.tngtech.archunit.lang.Priority;
+import com.tngtech.archunit.lang.SimpleConditionEvent;
 import jakarta.persistence.Entity;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeAll;
@@ -67,28 +73,50 @@ public class ArchitectureTest {
    */
   @Test
   public void noEntityInPublicInterfaceOfService() {
+
+
+
     DescribedPredicate<? super List<JavaClass>> entityTypeParam = new DescribedPredicate<List<JavaClass>>("Entity") {
       @Override
       public boolean test(List<JavaClass> javaClasses) {
         return javaClasses.stream()
-            .flatMap(cls -> cls.getAllInvolvedRawTypes().stream())
+            .flatMap(cls -> extractAllTypes(cls).stream())
             .filter(cls -> cls.isAnnotatedWith(Entity.class) || cls.isAssignableFrom(Persistable.class)).findAny()
             .isPresent();
       }
+
     };
     DescribedPredicate<? super JavaClass> entityReturnType = new DescribedPredicate<JavaClass>("Entity") {
       @Override
       public boolean test(JavaClass javaClass) {
-        Set<JavaClass> rawTypes = javaClass.getAllInvolvedRawTypes();
+        Set<JavaClass> rawTypes = extractAllTypes(javaClass);
         return rawTypes.stream()
             .filter(cls -> cls.isAnnotatedWith(Entity.class) || cls.isAssignableFrom(Persistable.class)).findAny()
             .isPresent();
       }
     };
+    ArchCondition<? super JavaMethod> methodPredicate = new ArchCondition<JavaMethod>("test") {
+      @Override
+      public void check(JavaMethod item, ConditionEvents events) {
+        var location = item.getSourceCodeLocation();
+        String sourceFileName = location.getSourceFileName();
+        int lineNumber = location.getLineNumber();
+        var sourceLink = String.format(" (%s:%d)", sourceFileName, lineNumber);
+        item.getAllInvolvedRawTypes().forEach(cls -> {
+          if(cls.isAnnotatedWith(Entity.class) || cls.isAssignableFrom(Persistable.class)) {
+            events.add(new SimpleConditionEvent(item, false, "Entity " + cls.getFullName() + " in public interface of @Service method " +item.getFullName()  + " > " + sourceLink));
+          }
+        });
+      }
+    };
     ArchRule rule = priority(HIGH).methods()
         .that().areDeclaredInClassesThat().areAnnotatedWith(Service.class).and().areNotPrivate()
-        .should().notHaveRawParameterTypes(entityTypeParam).andShould().notHaveRawReturnType(entityReturnType);
+        .should(methodPredicate);
     rule.check(importedClasses);
+  }
+
+  private static Set<JavaClass> extractAllTypes(JavaClass cls) {
+    return cls.getAllInvolvedRawTypes();
   }
 
   @Test
