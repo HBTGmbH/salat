c965a9c886e9d63822b43de5d5b42a81a1b29b9e
Klaus Richarz <kr@hbt.de>
2024-11-23 20:56:45 +0100

    Add TODO comment for future enhancement
    A TODO comment was added to the Warning class to consider migrating to a service feedback message. This is intended to prompt a future review and potential refactoring for better code organization.

--- src/main/java/org/tb/common/Warning.java
+++ src/main/java/org/tb/common/Warning.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
+// TODO consider migrate to service feedback message?
 public class Warning implements Serializable {
 
     private static final long serialVersionUID = 1L;
