7995251dd85e42965008b4d321a0e043a1771002
Klaus Richarz <kr@hbt.de>
2024-11-30 19:31:26 +0100

    Enable null conversion in Struts configuration
    Added the "convertNull" init parameter to the ActionServlet bean to ensure null values are properly converted. This enhances the handling of null parameters in Struts actions, improving consistency and reducing potential null-related errors.

--- src/main/java/org/tb/common/configuration/StrutsConfiguration.java
+++ src/main/java/org/tb/common/configuration/StrutsConfiguration.java
@@ -14,6 +14,7 @@ public class StrutsConfiguration {
   public ServletRegistrationBean<ActionServlet> actionServlet() {
     var bean = new ServletRegistrationBean<>(new ActionServlet());
     bean.addInitParameter("config", "/WEB-INF/struts-config.xml");
+    bean.addInitParameter("convertNull", "true");
     bean.setLoadOnStartup(1);
     bean.addUrlMappings("/do/*");
     return bean;
