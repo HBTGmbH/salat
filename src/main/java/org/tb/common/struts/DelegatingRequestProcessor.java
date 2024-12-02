dcace3a1cacb546e42f4f5cbe72a93b7f4ef1b9d
Klaus Richarz <kr@hbt.de>
2024-12-02 14:39:34 +0100

    Enhance forwarding with response caching and exception handling
    Implemented response caching in the doForward method using ContentCachingResponseWrapper to improve response handling. Added comprehensive exception handling by obtaining the ActionForm and processing exceptions, ensuring robust request processing. This change enhances both performance and stability in request forwarding scenarios.

--- src/main/java/org/tb/common/struts/DelegatingRequestProcessor.java
+++ src/main/java/org/tb/common/struts/DelegatingRequestProcessor.java
@@ -3,10 +3,14 @@ package org.tb.common.struts;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.struts.Globals;
 import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.ActionServlet;
 import org.apache.struts.action.RequestProcessor;
@@ -15,6 +19,7 @@ import org.springframework.beans.BeansException;
 import org.springframework.util.ClassUtils;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 import org.tb.common.SalatProperties;
 
 @Slf4j
@@ -61,4 +66,28 @@ public class DelegatingRequestProcessor extends RequestProcessor {
     return ClassUtils.forName(actionClassName, Thread.currentThread().getContextClassLoader());
   }
 
+  @Override
+  protected void doForward(String uri, HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+    try {
+      ContentCachingResponseWrapper wrapper = new ContentCachingResponseWrapper(response);
+      super.doForward(uri, request, wrapper);
+      wrapper.copyBodyToResponse();
+    } catch (Exception e) {
+      ActionMapping mapping = (ActionMapping) request.getAttribute(Globals.MAPPING_KEY);
+      ActionForm form = getActionForm(request, mapping);
+      ActionForward forward = processException(request, response, e, form, mapping);
+      if(forward != null) {
+        processForwardConfig(request, response, forward);
+      }
+    }
+  }
+
+  private static ActionForm getActionForm(HttpServletRequest request, ActionMapping mapping) {
+    if ("request".equals(mapping.getScope())) {
+      return (ActionForm) request.getAttribute(mapping.getAttribute());
+    }
+    HttpSession session = request.getSession();
+    return (ActionForm) session.getAttribute(mapping.getAttribute());
+  }
 }
