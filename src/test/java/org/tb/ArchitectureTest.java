2ead54b83ff6e9d65fc41b5894bffdb4e7d957b9
Klaus Richarz <kr@hbt.de>
2025-01-17 21:51:21 +0100

    Refactor architecture tests to use ArchUnit JUnit 5 syntax.
    Replaced custom test setup with ArchUnit's JUnit 5 annotations for improved clarity and maintainability. Updated dependencies in `pom.xml` to include `archunit-junit5` for proper support. Adjusted predicates and rules to simplify and enhance test readability.

--- src/test/java/org/tb/ArchitectureTest.java
+++ src/test/java/org/tb/ArchitectureTest.java
@@ -7,209 +7,118 @@ import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.sli
 
 import com.tngtech.archunit.base.DescribedPredicate;
 import com.tngtech.archunit.core.domain.JavaClass;
-import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.domain.JavaMethod;
-import com.tngtech.archunit.core.importer.ClassFileImporter;
-import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeGradleTestFixtures;
+import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeJars;
+import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchCondition;
 import com.tngtech.archunit.lang.ArchRule;
 import com.tngtech.archunit.lang.ConditionEvents;
 import com.tngtech.archunit.lang.SimpleConditionEvent;
 import jakarta.persistence.Entity;
 import java.util.Set;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.springframework.data.domain.Persistable;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 @Disabled
+@AnalyzeClasses(packages = "org.tb", importOptions = {DoNotIncludeTests.class, DoNotIncludeJars.class, DoNotIncludeGradleTestFixtures.class})
 public class ArchitectureTest {
 
-  private static JavaClasses importedClasses;
-
-  @BeforeAll
-  public static void loadClass() {
-    importedClasses = new ClassFileImporter()
-        .withImportOption(new ImportOption.DoNotIncludeTests())
-        .importPackages("org.tb");
-  }
-
-  @Test
-  public void daoMethodsMustStartWithGetOrFind() {
-    ArchRule rule = priority(MEDIUM).methods()
+  @ArchTest
+  static final ArchRule daoMethodsMustStartWithGetOrFind = priority(MEDIUM).methods()
         .that().areNotPrivate()
         .and().areDeclaredInClassesThat().haveSimpleNameEndingWith("DAO")
         .should().haveNameStartingWith("get")
         .orShould().haveNameStartingWith("find");
-    rule.check(importedClasses);
-  }
 
-  @Test
-  public void commonShouldNotAccessOtherSalatPackages() {
-    DescribedPredicate<? super JavaClass> predicate = new DescribedPredicate<>("common must import nothing") {
-      @Override
-      public boolean test(JavaClass javaClass) {
-        return javaClass.getFullName().startsWith("org.tb.") &&
-               !(
-                   javaClass.getFullName().startsWith("org.tb.common.")
-               );
-      }
-    };
-    ArchRule rule = priority(HIGH).noClasses().that()
-        .resideInAPackage("org.tb.common..")
-        .should().dependOnClassesThat(predicate);
-    rule.check(importedClasses);
-  }
+  @ArchTest
+  static final ArchRule commonShouldNotAccessOtherSalatPackages = priority(HIGH).noClasses().that()
+      .resideInAPackage("org.tb.common..")
+      .should().dependOnClassesThat(new OnlyOwnDependencyPredicate("common must import nothing", "org.tb.common."));
 
-  @Test
-  public void authShouldAccessCommonOnly() {
-    DescribedPredicate<? super JavaClass> predicate = new DescribedPredicate<>("auth must only import common") {
-      @Override
-      public boolean test(JavaClass javaClass) {
-        return javaClass.getFullName().startsWith("org.tb.") &&
-               !(
-                   javaClass.getFullName().startsWith("org.tb.common.") ||
-                   javaClass.getFullName().startsWith("org.tb.auth.")
-               );
-      }
-    };
-    ArchRule rule = priority(HIGH).noClasses().that()
-        .resideInAPackage("org.tb.auth..")
-        .should().dependOnClassesThat(predicate);
-    rule.check(importedClasses);
-  }
+  @ArchTest
+  static final ArchRule authShouldAccessCommonOnly = priority(HIGH).noClasses().that()
+      .resideInAPackage("org.tb.auth..")
+      .should().dependOnClassesThat(new OnlyOwnDependencyPredicate("auth must only import common", "org.tb.common.", "org.tb.auth."));
 
-  @Test
-  public void customerShouldAccessCommonAuthOnly() {
-    DescribedPredicate<? super JavaClass> predicate = new DescribedPredicate<>("customer must only import , auth") {
-      @Override
-      public boolean test(JavaClass javaClass) {
-        return javaClass.getFullName().startsWith("org.tb.") &&
-               !(
-                   javaClass.getFullName().startsWith("org.tb.common.") ||
-                   javaClass.getFullName().startsWith("org.tb.auth.") ||
-                   javaClass.getFullName().startsWith("org.tb.customer.")
-               );
-      }
-    };
-    ArchRule rule = priority(HIGH).noClasses().that()
-        .resideInAPackage("org.tb.customer..")
-        .should().dependOnClassesThat(predicate);
-    rule.check(importedClasses);
-  }
+  @ArchTest
+  static final ArchRule customerShouldAccessCommonAuthOnly = priority(HIGH).noClasses().that()
+      .resideInAPackage("org.tb.auth..")
+      .should().dependOnClassesThat(new OnlyOwnDependencyPredicate("customer must only import common, auth", "org.tb.common.", "org.tb.auth.", "org.tb.customer."));
 
-  @Test
-  public void employeeShouldAccessCommonAuthOnly() {
-    DescribedPredicate<? super JavaClass> predicate = new DescribedPredicate<>("employee must only import common, auth") {
-      @Override
-      public boolean test(JavaClass javaClass) {
-        return javaClass.getFullName().startsWith("org.tb.") &&
-               !(
-                   javaClass.getFullName().startsWith("org.tb.common.") ||
-                   javaClass.getFullName().startsWith("org.tb.auth.") ||
-                   javaClass.getFullName().startsWith("org.tb.employee.")
-               );
-      }
-    };
-    ArchRule rule = priority(HIGH).noClasses().that()
-        .resideInAPackage("org.tb.employee..")
-        .should().dependOnClassesThat(predicate);
-    rule.check(importedClasses);
-  }
+  @ArchTest
+  static final ArchRule employeeShouldAccessCommonAuthOnly = priority(HIGH).noClasses().that()
+      .resideInAPackage("org.tb.auth..")
+      .should().dependOnClassesThat(new OnlyOwnDependencyPredicate("employee must only import common, auth", "org.tb.common.", "org.tb.auth.", "org.tb.employee."));
 
-  @Test
-  public void orderShouldAccessCommonAuthCustomerEmployeeOnly() {
-    DescribedPredicate<? super JavaClass> predicate = new DescribedPredicate<>("order must only import common,auth,customer,employee") {
-      @Override
-      public boolean test(JavaClass javaClass) {
-        return javaClass.getFullName().startsWith("org.tb.") &&
-               !(
-                   javaClass.getFullName().startsWith("org.tb.common.") ||
-                   javaClass.getFullName().startsWith("org.tb.auth.") ||
-                   javaClass.getFullName().startsWith("org.tb.customer.") ||
-                   javaClass.getFullName().startsWith("org.tb.employee.") ||
-                   javaClass.getFullName().startsWith("org.tb.order.")
-               );
-      }
-    };
-    ArchRule rule = priority(HIGH).noClasses().that()
-        .resideInAPackage("org.tb.order..")
-        .should().dependOnClassesThat(predicate);
-    rule.check(importedClasses);
-  }
+  @ArchTest
+  static final ArchRule orderShouldAccessCommonAuthCustomerEmployeeOnly = priority(HIGH).noClasses().that()
+      .resideInAPackage("org.tb.auth..")
+      .should().dependOnClassesThat(new OnlyOwnDependencyPredicate("order must only import common,auth,customer,employee", "org.tb.common.", "org.tb.auth.", "org.tb.employee.", "org.tb.customer.", "org.tb.order."));
 
-  @Test
-  public void entitiesDoNotAccessServicesDAOsRepositories() {
-    ArchRule rule = priority(HIGH).noClasses().that()
-        .areAnnotatedWith(Entity.class)
-        .should().accessClassesThat().areAnnotatedWith(Repository.class)
-        .orShould().accessClassesThat().areAnnotatedWith(Service.class)
-        .orShould().accessClassesThat().haveSimpleNameEndingWith("DAO");
-    rule.check(importedClasses);
-  }
+  @ArchTest
+  static final ArchRule entitiesDoNotAccessServicesDAOsRepositories = priority(HIGH).noClasses().that()
+      .areAnnotatedWith(Entity.class)
+      .should().accessClassesThat().areAnnotatedWith(Repository.class)
+      .orShould().accessClassesThat().areAnnotatedWith(Service.class)
+      .orShould().accessClassesThat().haveSimpleNameEndingWith("DAO");
 
-  @Test
-  public void accessDataAccessObjectsOnlyInServices() {
-    ArchRule rule = priority(HIGH).noClasses()
+  @ArchTest
+  static final ArchRule accessDataAccessObjectsOnlyInServices = priority(HIGH).noClasses()
         .that().areNotAnnotatedWith(Service.class).and().haveSimpleNameNotEndingWith("DAO")
         .should().accessClassesThat().haveSimpleNameEndingWith("DAO");
-    rule.check(importedClasses);
-  }
 
-  @Test
-  public void accessRepositoriesOnlyInServicesOrDAOs() {
-    ArchRule rule = priority(HIGH).noClasses()
+  @ArchTest
+  static final ArchRule accessRepositoriesOnlyInServicesOrDAOs = priority(HIGH).noClasses()
         .that().areNotAnnotatedWith(Service.class).and().haveSimpleNameNotEndingWith("DAO")
         .should().accessClassesThat().areAnnotatedWith(Repository.class);
-    rule.check(importedClasses);
-  }
 
-  @Test
-  public void accessEntitiesOnlyInServicesOrDAOs() {
-    ArchRule rule = priority(HIGH).noClasses()
+  @ArchTest
+  static final ArchRule accessEntitiesOnlyInServicesOrDAOs = priority(HIGH).noClasses()
         .that().areNotAnnotatedWith(Entity.class)
         .and().areNotAnnotatedWith(Repository.class)
         .and().areNotAnnotatedWith(Service.class)
         .and().haveSimpleNameNotEndingWith("DAO")
         .should().accessClassesThat().areAnnotatedWith(Entity.class);
-    rule.check(importedClasses);
-  }
 
-  /*
-   * Generics werden nicht unterstützt.
-   */
-  @Test
-  public void noEntityInPublicInterfaceOfService() {
-    ArchCondition<? super JavaMethod> methodPredicate = new ArchCondition<JavaMethod>("test") {
-      @Override
-      public void check(JavaMethod item, ConditionEvents events) {
-        var location = item.getSourceCodeLocation();
-        String sourceFileName = location.getSourceFileName();
-        int lineNumber = location.getLineNumber();
-        var sourceLink = String.format(" (%s:%d)", sourceFileName, lineNumber);
-        item.getAllInvolvedRawTypes().forEach(cls -> {
-          if(cls.isAnnotatedWith(Entity.class) || cls.isAssignableFrom(Persistable.class)) {
-            events.add(new SimpleConditionEvent(item, false, "Entity " + cls.getFullName() + " in public interface of @Service method " +item.getFullName()  + " > " + sourceLink));
-          }
-        });
-      }
-    };
-    ArchRule rule = priority(HIGH).methods()
-        .that().areDeclaredInClassesThat().areAnnotatedWith(Service.class).and().areNotPrivate()
-        .should(methodPredicate);
-    rule.check(importedClasses);
-  }
+  @ArchTest
+  static final ArchRule noEntityInPublicInterfaceOfService = priority(HIGH).methods()
+      .that().areDeclaredInClassesThat().areAnnotatedWith(Service.class).and().areNotPrivate()
+      .should(new ArchCondition<JavaMethod>("not have JPA entities in their signature") {
+        @Override
+        public void check(JavaMethod item, ConditionEvents events) {
+          var location = item.getSourceCodeLocation();
+          String sourceFileName = location.getSourceFileName();
+          int lineNumber = location.getLineNumber() - 1;
+          var sourceLink = String.format(" (%s:%d)", sourceFileName, lineNumber);
+          item.getAllInvolvedRawTypes().forEach(cls -> {
+            if(cls.isAnnotatedWith(Entity.class) || cls.isAssignableFrom(Persistable.class)) {
+              events.add(new SimpleConditionEvent(item, false, "Entity " + cls.getFullName() + " in public interface of @Service method " +item.getFullName()  + " > " + sourceLink));
+            }
+          });
+        }
+      });
 
-  private static Set<JavaClass> extractAllTypes(JavaClass cls) {
-    return cls.getAllInvolvedRawTypes();
-  }
+  @ArchTest
+  static final ArchRule beFreeOfCycles = slices().matching("org.tb.(*)..").should().beFreeOfCycles();
 
-  @Test
-  public void beFreeOfCycles() {
-    ArchRule rule = slices().matching("org.tb.(*)..").should().beFreeOfCycles();
-    rule.check(importedClasses);
+  private static class OnlyOwnDependencyPredicate extends DescribedPredicate<JavaClass> {
+
+    private final Set<String> notFiringPackagePrefixes;
+
+    public OnlyOwnDependencyPredicate(String description, String... notFiringPackagePrefixes) {
+      super(description);
+      this.notFiringPackagePrefixes = Set.of(notFiringPackagePrefixes);
+    }
+
+    @Override
+    public boolean test(JavaClass javaClass) {
+      return javaClass.getFullName().startsWith("org.tb.") && !notFiringPackagePrefixes.stream().anyMatch(javaClass.getFullName()::startsWith);
+    }
   }
 
 }
