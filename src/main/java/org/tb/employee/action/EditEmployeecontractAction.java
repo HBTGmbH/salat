fb763b1516e9a5ee1af999ee7d071297580f5484
Klaus Richarz <kr@hbt.de>
2025-06-13 15:20:57 +0200

    Remove unused static import from `EditEmployeecontractAction`

--- src/main/java/org/tb/employee/action/EditEmployeecontractAction.java
+++ src/main/java/org/tb/employee/action/EditEmployeecontractAction.java
@@ -1,7 +1,5 @@
 package org.tb.employee.action;
 
-import static org.tb.common.util.DateUtils.today;
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.time.Duration;
