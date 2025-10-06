711dded5ff49de24ea8c7f4123f4ec7d1adc909e
Klaus Richarz <kr@hbt.de>
2025-10-06 13:50:05 +0200

    #540 Simplify `AzureEasyAuthSecurityConfiguration` by removing custom logout functionality and unused imports

--- src/main/java/org/tb/auth/configuration/AzureEasyAuthSecurityConfiguration.java
+++ src/main/java/org/tb/auth/configuration/AzureEasyAuthSecurityConfiguration.java
@@ -1,11 +1,6 @@
 package org.tb.auth.configuration;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -20,8 +15,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.web.BearerTokenResolver;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.util.Assert;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -103,42 +96,9 @@ public class AzureEasyAuthSecurityConfiguration {
   public SecurityFilterChain filterChain(HttpSecurity http, SalatProperties salatProperties) throws Exception {
     http.authorizeHttpRequests(authz -> authz.anyRequest().authenticated())
         .oauth2ResourceServer(oauth2 -> oauth2.jwt())
-        .logout(logout -> logout.logoutRequestMatcher(logoutRequestMatcher(salatProperties)).addLogoutHandler(logoutHandler()))
         .cors().disable()
         .csrf().disable();
     return http.build();
-  }
-
-  private LogoutHandler logoutHandler() {
-    return (request, response, auth) -> {
-      Cookie[] cookies = request.getCookies();
-      for(Cookie cookie : cookies) {
-        cookie.setValue("");
-        cookie.setPath("/");
-        cookie.setMaxAge(0);
-        cookie.setSecure(true);
-        cookie.setComment("EXPIRING COOKIE at " + LocalDateTime.now());
-        response.addCookie(cookie);
-      }
-      try {
-        response.sendRedirect("/");
-      } catch (IOException e) {
-        throw new RuntimeException("Could not send redirect to /", e);
-      }
-    };
-  }
-
-  private RequestMatcher logoutRequestMatcher(SalatProperties salatProperties) {
-    // a logout is required if no id token is provided any more or if the access token expires in the next 2 minutes or has been expired
-    return (request) -> {
-      boolean appServiceAuthSessionPresent = request.getCookies() != null && Arrays.stream(request.getCookies()).anyMatch(c -> c.getName().equalsIgnoreCase("AppServiceAuthSession"));
-      boolean oidcTokenPresent = request.getHeader(salatProperties.getAuth().getOidcIdToken().getHeaderName()) != null;
-      boolean accessTokenPresent = request.getHeader(salatProperties.getAuth().getAccessToken().getHeaderName()) != null;
-      String accessTokenExpiresOn = request.getHeader(salatProperties.getAuth().getAccessToken().getExpiresOnHeaderName());
-      OffsetDateTime expires = accessTokenExpiresOn != null ? OffsetDateTime.parse(accessTokenExpiresOn) : OffsetDateTime.MAX;
-      boolean accessTokenExpired = accessTokenPresent && expires.isBefore(OffsetDateTime.now().plusMinutes(2)); // grace period of 2 minutes
-      return appServiceAuthSessionPresent && (!oidcTokenPresent || accessTokenExpired);
-    };
   }
 
   @Bean
