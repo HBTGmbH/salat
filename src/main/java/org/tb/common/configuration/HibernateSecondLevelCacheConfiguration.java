bdef4a16f7aad62f19e72afa4c920edc3a69c9bc
Klaus Richarz <kr@hbt.de>
2025-11-21 15:59:40 +0100

    Replace incorrect `HibernatePropertiesCustomizer` import with proper `springframework.boot` package path

--- src/main/java/org/tb/common/configuration/HibernateSecondLevelCacheConfiguration.java
+++ src/main/java/org/tb/common/configuration/HibernateSecondLevelCacheConfiguration.java
@@ -7,7 +7,7 @@ import javax.cache.Caching;
 import javax.cache.spi.CachingProvider;
 import org.hibernate.cache.jcache.ConfigSettings;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
+import org.springframework.boot.hibernate.autoconfigure.HibernatePropertiesCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ResourceLoader;
