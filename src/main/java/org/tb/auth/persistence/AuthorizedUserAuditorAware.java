364ed19f18d607c3d609b83158b5af376f641a0e
Klaus Richarz <kr@hbt.de>
2026-01-06 22:28:33 +0100

    Refactor `AuthorizedUserAuditorAware` to enhance flexibility in retrieving authorized user
    - Replaced direct dependency injection of `AuthorizedUser` with `BeanFactory` to improve decoupling
    - Introduced `getAuthorizedUserIfPresent` method to safely retrieve authorized user from context
    - Improved handling of scenarios with no active request or session

--- src/main/java/org/tb/auth/persistence/AuthorizedUserAuditorAware.java
+++ src/main/java/org/tb/auth/persistence/AuthorizedUserAuditorAware.java
@@ -4,22 +4,35 @@ import static org.tb.common.GlobalConstants.SYSTEM_SIGN;
 
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
 import org.tb.auth.domain.AuthorizedUser;
 
 @Component
 @RequiredArgsConstructor
 public class AuthorizedUserAuditorAware implements AuditorAware<String> {
 
-  private final AuthorizedUser authorizedUser;
+  private final BeanFactory beanFactory;
 
   @Override
   public Optional<String> getCurrentAuditor() {
-    if(authorizedUser.isAuthenticated()) {
-      return Optional.of(authorizedUser.getLoginSign());
+    var authorizedUser = getAuthorizedUserIfPresent();
+    if(authorizedUser.isPresent() && authorizedUser.get().isAuthenticated()) {
+      return Optional.of(authorizedUser.get().getLoginSign());
     }
     return Optional.of(SYSTEM_SIGN);
+  }
+
+  public Optional<AuthorizedUser> getAuthorizedUserIfPresent() {
+    RequestAttributes attrs = RequestContextHolder.getRequestAttributes();
+    if (attrs == null) {
+      return Optional.empty(); // no request → no session
+    }
+
+    return Optional.of(beanFactory.getBean(AuthorizedUser.class));
   }
 
 }
