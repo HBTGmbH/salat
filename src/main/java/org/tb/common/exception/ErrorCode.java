5a3fc1042e9b127178a33eaeabd63de49db8daa3
Klaus Richarz <klaus.richarz@hbt.de>
2026-09-25 19:14:37 +0200

    #1112 - Nicht lesbare Werte des CSV-Imports als Eingabefehler melden

--- src/main/java/org/tb/common/exception/ErrorCode.java
+++ src/main/java/org/tb/common/exception/ErrorCode.java
@@ -88,6 +88,8 @@ public enum ErrorCode {
   TR_DURATION_INVALID_FORMAT("TR-0031", "duration could not be interpreted"),
   TR_TICKET_REFERENCE_INVALID_LENGTH("TR-0032", "ticket reference is too long"),
   TR_SERIAL_DAYS_OUT_OF_RANGE("TR-0033", "number of serial days out of valid range"),
+  TR_CSV_VALUE_FORMAT_INVALID("TR-0034", "a value of the uploaded CSV file matches none of the expected formats"),
+  TR_CSV_LINE_NOT_READABLE("TR-0035", "a line of the uploaded CSV file could not be read"),
 
   RL_RELEASE_NOT_ALLOWED("RL-0001", "release not allowed"),
   RL_ACCEPT_NOT_ALLOWED("RL-0002", "accept not allowed"),
